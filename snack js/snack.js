09b069c973c801fa14c956a4efb5e7ed1088bbc4
armandotruglio <armando.truglio1@gmail.com>
2024-09-02 12:31:12 +0200

    Snack 7 completato

--- snack js/snack.js
+++ snack js/snack.js
@@ -27,3 +27,69 @@ const mammals = animals.filter(animal => animal.classe === 'mammiferi');
 
 //SNACK 7
 
+/* Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un'età.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età. */
+
+const people = [
+    {
+        firstName: 'Armando',
+        lastName: 'Truglio',
+        age: 24
+    },
+    {
+        firstName: 'Tizio',
+        lastName: 'Caio',
+        age: 43
+    },
+    {
+        firstName: 'Giovanni',
+        lastName: 'Sempronio',
+        age: 16
+    },
+    {
+        firstName: 'Francesco',
+        lastName: 'Rossi',
+        age: 20
+    },
+    {
+        firstName: 'Tiziano',
+        lastName: 'Ferro',
+        age: 50
+    },
+    {
+        firstName: 'Emilio',
+        lastName: 'Bianchi',
+        age: 5
+    },
+    {
+        firstName: 'Marco',
+        lastName: 'Verdi',
+        age: 14
+    },
+    {
+        firstName: 'Antonio',
+        lastName: 'Primi',
+        age: 90
+    },
+    {
+        firstName: 'Ernesto',
+        lastName: 'Presti',
+        age: 36
+    },
+    {
+        firstName: 'Emiliano',
+        lastName: 'Carrello',
+        age: 17
+    },
+]
+
+const drivers = people.reduce((acc, person) => {
+    if (person.age >= 18) 
+        acc.push(`Il sig. ${person.lastName} ${person.firstName} è abilitato a conseguire una patente di guida in quanto maggiorenne`);
+    return acc;
+}, []);
+
+console.log(drivers);
+
